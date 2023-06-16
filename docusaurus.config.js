1efc25072364f78e46b4e28b4fb4806546bd9cb2
temz <artem.rusanov.02@gmail.com>
2023-06-16 12:45:02 +0700

    feat: initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,75 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'NX Docs',
+  tagline: 'NX useful libs and tools',
+  url: 'https://temarusanov.github.io',
+  baseUrl: '/dev-notes/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'temarusanov', // Usually your GitHub org/user name.
+  projectName: 'dev-notes', // Usually your repo name.
+  trailingSlash: false,
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ru'],
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/', // Serve the docs at the site's root
+          /* other docs plugin options */
+        },
+        blog: false, // Optional: disable the blog plugin
+        // ...
+      },
+    ],
+  ],
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Tema docs',
+        logo: {
+          alt: 'Docs logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'home',
+            position: 'left',
+            label: 'Docs',
+          },
+          {
+            type: 'localeDropdown',
+            position: 'left',
+          },
+          {
+            href: 'https://github.com/temarusanov/dev-notes',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()}. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
