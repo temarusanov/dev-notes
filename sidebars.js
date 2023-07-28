522713a57e7d7a7c6ab8b3e0f18dec0e0434f851
temz <artem.rusanov.02@gmail.com>
2023-07-28 23:20:31 +0700

    feat: add logging technique

--- sidebars.js
+++ sidebars.js
@@ -29,7 +29,7 @@ const sidebars = {
             slug: '/workspace/techniques',
             keywords: ['workspace', 'nx', 'boilerplate'],
           },
-          items: ['workspace/techniques/dependency-injection', 'workspace/techniques/health-checks']
+          items: ['workspace/techniques/dependency-injection', 'workspace/techniques/health-checks', 'workspace/techniques/logging']
         },
         ],
     },
